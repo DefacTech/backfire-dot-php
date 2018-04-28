353613a0c08b35efcf958bc71002e371df40d7a9
Da140n <38798088+Da140n@users.noreply.github.com>
2018-04-28 01:11:22 +0000

    Create backfire-en.php

---
+++ backfire-en.php
@@ -0,0 +1,45 @@
+<!TITLE- Backfire Filter dot PHP!>
+<!Author- Dalton Hill, other, other, other!>
+<!Translator- none!>
+<!We are in need of some ideas. This file must be in plain English. If you wish to recreate Backfire in other languages, that would be great. We prefer it to be backfire-es.php, backfire-fr.php, and so on. 
+
+<?php
+if( strpos($_POST["user_input"],"he is a nigger","he's a nigger","she is a nigger","she's a nigger","kill all niggers","he is a cracker","he's a cracker","she is a cracker","she's a cracker","kill all crackers","he is a wetback","he's a wetback","she is a wetback","she's a wetback","kill all wetbacks")!==FALSE ) {
+    echo("I'm a racist motherfucker, so I will hang myself by the genitals and nipples from a tree with meat hooks" );
+}
+
+if( strpos($_POST["user_input"],"faggot","you are a faggot","he is a faggot","he's a faggot","she is a faggot","she's a faggot","they are faggots","they're faggots")!==FALSE ) {
+    echo("I want someone of the same gender to slip me some tongue and fuck me in the ass with a rubber dick and cum in in my mouth" );
+}
+
+if( strpos($_POST["user_input"],"nigga")!==FALSE ) {
+    echo(">>>Hold on just a fucking minute! If it's not okay for a white person to say 'nigga', then why in the fuck should you say it?! Don't get started with that slavery and segregation shit, because most of us wasn't born at that time.<<<" );
+}
+
+if( strpos($_POST["user_input"],"a cunt","a whore",)!==FALSE ) {
+    echo("the sexiest person I've ever met in my life" );
+}
+
+if( strpos($_POST["user_input"],"cunts","whores")!==FALSE ) {
+    echo("the sexiest people I've ever met in my life" );
+}
+
+if( strpos($_POST["user_input"],"Go back to Africa","Go back to Africa, Nigger",)!==FALSE ) {
+    echo("That's it! I'm going back to Europe" );
+}
+if( strpos($_POST["user_input"],"you should go back to Africa","you should go back to Africa, Nigger",)!==FALSE ) {
+    echo("I should probably go back to Europe" );
+}
+if( strpos($_POST["user_input"],"",)!==FALSE ) {
+    echo("" );
+}
+if( strpos($_POST["user_input"],"",)!==FALSE ) {
+    echo("" );
+}
+if( strpos($_POST["user_input"],"",)!==FALSE ) {
+    echo("" );
+}
+if( strpos($_POST["user_input"],"",)!==FALSE ) {
+    echo("" );
+}
+?>
