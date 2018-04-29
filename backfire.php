fc7e19731d6db60ca3f46220f3dfc77a4f115f90
Da140n <38798088+Da140n@users.noreply.github.com>
2018-04-29 02:13:59 +0000

    Create backfire.php

---
+++ backfire.php
@@ -0,0 +1,27 @@
+<! This this for multiple files in different languages. There is no need to edit it unless you want to add another language. !>
+<! We'll need every major language we can get. 
+<! Everything is in alphabetical order and grouped in families (if that's what they're called). !>
+
+<?php 
+/* Germanic Languages */
+include "./lang/backfire-de.php"; /*deutsch*/
+include "./lang/backfire-en.php"; /*english*/
+include "./lang/backfire-fi.php"; /*suomalainen*/
+include "./lang/backfire-pl.php"; /*polskie*/
+
+/* Romance Languages */
+include "./lang/backfire-ca.php"; /* català */
+include "./lang/backfire-es.php"; /*español*/
+include "./lang/backfire-fr.php"; /*français*/
+include "./lang/backfire-it.php"; /*italiano*/
+include "./lang/backfire-ro.php"; /*română*/
+
+/* Slavic Languages */
+include "./lang/backfire-ru.php"; /*русский*/
+
+/* Semitic Languages */
+include "./lang/backfire-ar.php"; /*عربى*/
+include "./lang/backfire-fa.php"; /*فارسی*/
+include "./lang/backfire-he.php"; /*עִברִית*/
+
+?>
