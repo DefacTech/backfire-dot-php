a277edc980fdc645baf021189f0041ee0be4a177
Da140n <38798088+Da140n@users.noreply.github.com>
2018-05-03 00:58:32 +0000

    Update backfire.php

--- backfire.php
+++ backfire.php
@@ -1,5 +1,5 @@
 <! This this for multiple files in different languages. There is no need to edit it unless you want to add another language. !>
-<! We'll need every major language we can get. 
+<! We'll need every major language we can get. !>
 <! Everything is in alphabetical order and grouped in families (if that's what they're called). !>
 
 <?php 
@@ -7,7 +7,6 @@
 include "./lang/backfire-de.php"; /*deutsch*/
 include "./lang/backfire-en.php"; /*english*/
 include "./lang/backfire-fi.php"; /*suomalainen*/
-include "./lang/backfire-pl.php"; /*polskie*/
 
 /* Romance Languages */
 include "./lang/backfire-ca.php"; /* català */
@@ -17,11 +16,21 @@ include "./lang/backfire-it.php"; /*italiano*/
 include "./lang/backfire-ro.php"; /*română*/
 
 /* Slavic Languages */
+include "./lang/backfire-cs.php"; /*čeština*/
 include "./lang/backfire-ru.php"; /*русский*/
+include "./lang/backfire-pl.php"; /*polskie*/
+include "./lang/backfire-sk.php"; /*slovenský*/
+include "./lang/backfire-ua.php"; /*Українська*/
 
 /* Semitic Languages */
 include "./lang/backfire-ar.php"; /*عربى*/
 include "./lang/backfire-fa.php"; /*فارسی*/
 include "./lang/backfire-he.php"; /*עִברִית*/
+include "./lang/backfire-ur.php"; /*اردو*/
 
+/* Asian Languages */
+include "./lang/backfire-ja.php"; /*日本語*/
+include "./lang/backfire-ko.php"; /*한국어*/
+include "./lang/backfire-mn.php"; /*Монгол хэл*/
+include "./lang/backfire-hi.php"; /*हिंदी*/
 ?>
