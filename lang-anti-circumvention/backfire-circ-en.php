c432eb904b9a9a69b755099a5f8a0c8315809d45
Da140n <38798088+Da140n@users.noreply.github.com>
2018-05-04 10:01:31 +0000

    Update backfire-circ-en.php

--- lang-anti-circumvention/backfire-circ-en.php
+++ lang-anti-circumvention/backfire-circ-en.php
@@ -1,1 +1,19 @@
+<! This file will be designed to be circumvention resistant. It won't stop all forms of circumvention, but it's a start !>
+<! If you know what you're doing, try to figure out every possible way you would circumvent censorship on Facebook to avoid Facebook Jail. !>
 
+
+if( strpos($_POST["user_input"],"",)!==FALSE ) {
+    echo("" );
+}
+
+if( strpos($_POST["user_input"],"",)!==FALSE ) {
+    echo("" );
+}
+
+if( strpos($_POST["user_input"],"",)!==FALSE ) {
+    echo("" );
+}
+
+if( strpos($_POST["user_input"],"",)!==FALSE ) {
+    echo("" );
+}
