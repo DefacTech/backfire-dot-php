34865bd9f34a19b5c12fef1005cda1c7c8bcb9d1
Da140n <38798088+Da140n@users.noreply.github.com>
2018-05-16 21:17:58 -0500

    Update backfire-en.php

--- lang-correct-grammer/backfire-en.php
+++ lang-correct-grammer/backfire-en.php
@@ -2,50 +2,38 @@
 <!Author- Dalton Hill, other, other, other!>
 <!Translator- none!>
 <!We are in need of some ideas. This file must be in plain English. If you wish to recreate Backfire in other languages, that would be great. We prefer it to be backfire-es.php, backfire-fr.php, and so on. You'll see the filenames in backfire.php !>
-
 <?php
 if( strpos($_POST["user_input"],"he is a nigger","he's a nigger","she is a nigger","she's a nigger","kill all niggers","he is a cracker","he's a cracker","she is a cracker","she's a cracker","kill all crackers","he is a wetback","he's a wetback","she is a wetback","she's a wetback","kill all wetbacks")!==FALSE ) {
     echo("I'm a racist motherfucker, so I will hang myself by the genitals and nipples from a tree with meat hooks" );
 }
-
 if( strpos($_POST["user_input"],"faggot","you are a faggot","he is a faggot","he's a faggot","she is a faggot","she's a faggot","they are faggots","they're faggots")!==FALSE ) {
     echo("I want someone of the same gender to slip me some tongue and fuck me in the ass with a rubber dick and cum in in my mouth" );
 }
-
 if( strpos($_POST["user_input"],"nigga")!==FALSE ) {
-    echo(">>>Hold on just a fucking minute! If it's not okay for a white person to say 'nigga', then why in the fuck should you say it?! Don't get started with that slavery and segregation shit, because most of us wasn't born at that time.<<<" );
+    echo(">>>Hold on just a fucking minute! If it's not okay for a white person to say 'nigga', then why in the fuck should you say it?! Don't get started with that slavery and segregation shit, because most of us wasn't born at that time.<<<"  strpos($_POST["user_input"],"a cunt","a whore",)!==FALSE ) {
 }
-
-if( strpos($_POST["user_input"],"a cunt","a whore",)!==FALSE ) {
+if( strpos($_POST["user_input"],"a cunt","a whore")!==FALSE ) {
     echo("the sexiest person I've ever met in my life" );
 }
-
 if( strpos($_POST["user_input"],"cunts","whores")!==FALSE ) {
     echo("the sexiest people I've ever met in my life" );
 }
-
 /*We are not what you call anti-Semitic, we're just trying to help you enforce equality. Besides, what's so bad about hating a family of languages? Don't worry, Jews and Christians will get their share, too.*/
-if( strpos($_POST["user_input"],"All Muslims are terrorists",)!==FALSE ) {
+if( strpos($_POST["user_input"],"All Muslims are terrorists","Fuck all Muslims","Fuck Muslims","Fuck sand niggers")!==FALSE ) {
     echo("All Muslims are terrortists. And using my logic, if all Muslims are terrorists, then all Christians are bigoted assholes, all Hindu men and lesbians ask for bob and vagene, and all Jews are some greedy motherfuckers" );
 }
-/*Why isn't Atheism included here? Because basically we're accused of being evil and having no morals over the simple fact we don't believe a supreme being exists.*/
-
-if( strpos($_POST["user_input"],"Go back to Africa","Go back to Africa, Nigger",)!==FALSE ) {
+if( strpos($_POST["user_input"],"Fuck all Jews","Fuck Jews","Fuck kikes")!==FALSE ) {
+    echo("Fuck Jews for being greedy and privileged, fuck Christians and their bigotted and pedophile ways, Fuck Muslims for blowing people up, and fuck Hindus for asking bob and vagene on social media" );
+}
+/*We can't figure anything out for Christians yet. If you have ideas, let us know.*/
+if( strpos($_POST["user_input"],"Go back to Africa","Go back to Africa, Nigger","Go back to Mexico","Go back to Mexico, Wetback")!==FALSE ) {
     echo("That's it! I'm going back to Europe" );
 }
-if( strpos($_POST["user_input"],"you should go back to Africa","you should go back to Africa, Nigger",)!==FALSE ) {
+if( strpos($_POST["user_input"],"you should go back to Africa","you should go back to Africa, Nigger","You should go back to Mexico","You should go back to Mexico, Wetback")!==FALSE ) {
     echo("I should probably go back to Europe" );
-}
+/* Copy and paste the three lines below this one
 if( strpos($_POST["user_input"],"",)!==FALSE ) {
     echo("" );
 }
-if( strpos($_POST["user_input"],"",)!==FALSE ) {
-    echo("" );
-}
-if( strpos($_POST["user_input"],"",)!==FALSE ) {
-    echo("" );
-}
-if( strpos($_POST["user_input"],"",)!==FALSE ) {
-    echo("" );
-}
+*/
 ?>
